da7dbabd708390dcbd9db8ca01122f470f6d2c31
Hunter <hunterde91@gmail.com>
2024-06-04 21:03:45 -0400

    added navbar to page and confirmed its working

--- reactPortfolio/src/App.jsx
+++ reactPortfolio/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
-
+import Nav from './components/navbar'
 
 function App() {
   
   return (
     <>
-     
+     <Nav />
       <main className="mx-3">
         <Outlet />
       </main>
