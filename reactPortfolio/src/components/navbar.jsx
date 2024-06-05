6b6c0e6f3fcb24cc1b5ad0cc1a7e5719e67c7d6e
Hunter <hunterde91@gmail.com>
2024-06-04 22:11:47 -0400

    fixed navbar so the link is highlighted when on page

--- reactPortfolio/src/components/navbar.jsx
+++ reactPortfolio/src/components/navbar.jsx
@@ -1,31 +1,80 @@
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Nav() {
-    // The Navbar UI component will render each of the Link elements in the links prop
-    return (
-        <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/projects">Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/resume">Resume</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        </>
-    )
-  }
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  onClick={() => handlePageChange('About')}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/"
+                  end
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  onClick={() => handlePageChange('Projects')}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/projects"
+                >
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  onClick={() => handlePageChange('Contact')}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/contact"
+                >
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  onClick={() => handlePageChange('Resume')}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/resume"
+                >
+                  Resume
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
