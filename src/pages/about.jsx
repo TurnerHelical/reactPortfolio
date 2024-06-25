8ff7357557ba1ad7c6cbd6c0941d802b894fb62f
Hunter <hunterde91@gmail.com>
2024-06-24 22:17:23 -0400

    edited the home page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,40 +1,51 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import logo from '../assets/Dark_Logo.png';
+
 export default function AboutPage() {
-    return (
-      <main>
-        <h2>WELCOME TO MY SITE</h2>
-          <p>Thanks for visiting my site! My name is Hunter LeClair and I'm a full stack developer. I recently completed the UCF Coding Bootcamp and I'm searching for my start in web development. Feel free to look around my site and view previous projects, my resume, or contact me if you have any questions. Thanks!
-          </p>
-          <h3>Technologies I Use</h3>
-          <p>I'm confindent in the listed Technologies but know that there is always more to learn and look forward to getting better and discovering new ones to use.</p>
-          <h4>Frontend</h4>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>REACT</li>
-            <li>Bootstrap</li>
-            <li>MUI</li>
-            <li>and More</li>
-          </ul>
-          <h4>Backend</h4>
-          <ul>
-            <li>Node js</li>
-            <li>Express js</li>
-            <li>Postgres</li>
-            <li>MongoDB</li>
-            <li>GraphQL</li>
-            <li>and More</li>
-          </ul>
-          <h3>About Me</h3>
-          <h4>Personal Life</h4>
-          <p>I'm a born and raised Floridian and still live here with my kids. Like any good Floridian when it's the weekend I'm most likely at the beach or on the water. I'm a big sports guy and enjoy most sports but my favorite is College Football with FSU being my team. My other teams are:</p>
-          <ul>
-            <li>Rays</li>
-            <li>Buccaneers</li>
-            <li>Lightning</li>
-          </ul>
-          
-      </main>
-    )
-  }
-  
+  return (
+    <Container >
+      <Row className="align-items-center justify-content-center">
+        <Col xs={12} md={5} lg={4} className="image-col">
+            <Image src={logo} alt="Logo" fluid />
+        </Col>
+        <Col xs={12} md={7} lg={6} className="text-col">
+          <h2>WELCOME TO MY SITE</h2>
+          <p>Thanks for visiting my site! My name is Hunter LeClair and I'm a full stack developer. I recently completed the UCF Coding Bootcamp and I'm searching for my start in web development. Feel free to look around my site and view previous projects, my resume, or contact me if you have any questions. Thanks!</p>
+        </Col>
+      </Row>
+      <h3>Technologies I Use</h3>
+      <p>I'm confindent in the listed Technologies but know that there is always more to learn and look forward to getting better and discovering new ones to use.</p>
+      <h4>Frontend</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>REACT</li>
+        <li>Bootstrap</li>
+        <li>MUI</li>
+        <li>and More</li>
+      </ul>
+      <h4>Backend</h4>
+      <ul>
+        <li>Node js</li>
+        <li>Express js</li>
+        <li>Postgres</li>
+        <li>MongoDB</li>
+        <li>GraphQL</li>
+        <li>and More</li>
+      </ul>
+      <h3>About Me</h3>
+      <h4>Personal Life</h4>
+      <p>I'm a born and raised Floridian and still live here with my kids. Like any good Floridian when it's the weekend I'm most likely at the beach or on the water. I'm a big sports guy and enjoy most sports but my favorite is College Football with FSU being my team. My other teams are:</p>
+      <ul>
+        <li>Rays</li>
+        <li>Buccaneers</li>
+        <li>Lightning</li>
+      </ul>
+
+    </Container>
+  )
+}
